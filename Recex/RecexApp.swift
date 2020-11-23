0e37a840ef68d9e85f12a490daf18b3e13e11015
Pulkith Paruchuri <ballistic.minecrafter@gmail.com>
2020-11-23 14:00:37 -0600

    Initial Commit

---
+++ Recex/RecexApp.swift
@@ -0,0 +1,20 @@
+//
+//  RecexApp.swift
+//  Recex
+//
+//  Created by Pulkith Paruchuri on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct RecexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
